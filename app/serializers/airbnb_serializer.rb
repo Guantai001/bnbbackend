b95891611f10ac477cda818236fe7dcf33bdc31c
GUANTAI001 <jpguantai@gmail.com>
2023-06-05 11:29:37 +0300

    selilizer

--- app/serializers/airbnb_serializer.rb
+++ app/serializers/airbnb_serializer.rb
@@ -1,5 +1,5 @@
 class AirbnbSerializer < ActiveModel::Serializer
-  attributes :id, :admin_id, :name
+  attributes :id, :admin_id, :name, :location, :price, :beds, :description, :category
   has_many :airbnb_images
   has_many :amenities
 end
