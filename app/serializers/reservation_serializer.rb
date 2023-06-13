26a680bbc6473d0ef221d50ea09f41a6a86bdfeb
GUANTAI001 <jpguantai@gmail.com>
2023-06-13 19:34:08 +0300

    init

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,3 +1,4 @@
 class ReservationSerializer < ActiveModel::Serializer
   attributes :id, :from_date, :to_date, :airbnb_id, :user_id, :estimated_amount, :difference_in_nights
+  belongs_to :airbnb
 end
