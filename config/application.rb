e23b6c3ee81583af6ac41d37f42dfccf2818ff7d
GUANTAI001 <jpguantai@gmail.com>
2023-06-15 12:06:37 +0300

    mpesa intergration

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require_relative "boot"
 
 require "rails/all"
+require "rails/all"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +24,14 @@ module Bnbbackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.before_configuration do
+      env_file = Rails.root.join("config", "local_env.yml")
+      if File.exist?(env_file)
+        YAML.load_file(env_file).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,11 +39,11 @@ module Bnbbackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.before_configuration do
-      env_file = File.join(Rails.root, "config", "local_env.yml")
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exist?(env_file)
-    end
+    # config.before_configuration do
+    #   env_file = File.join(Rails.root, "config", "local_env.yml")
+    #   YAML.load(File.open(env_file)).each do |key, value|
+    #     ENV[key.to_s] = value
+    #   end if File.exist?(env_file)
+    # end
   end
 end
